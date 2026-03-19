b798a19833c892880cb340dbd10f1b1b27cb0e12
Satyam Pandey <100727142+Satyam-AI@users.noreply.github.com>
2026-03-19 18:26:14 +0530

    19-22.cpp solved/Revised

---
+++ Assignment - 8 - Striver's Pattern/20.cpp
@@ -0,0 +1,65 @@
+/*
+
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 1; j <= 2 * n - 2 * i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 1; j <= 2 * i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        pattern(n);
+    }
+    return 0;
+}
