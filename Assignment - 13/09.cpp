24d32fa17a9f7c59a3892aa9d24c5b5e9814ce87
Satyam Pandey <100727142+Satyam-AI@users.noreply.github.com>
2026-04-05 23:56:20 +0530

    Revised array/recursion

--- Assignment - 13/09.cpp
+++ Assignment - 13/09.cpp
@@ -11,10 +11,19 @@ int digitcount(int n, int count)
     return digitcount(n / 10, count + 1);
 }
 
+int digitcount2(int n, int count)
+{
+    if (n == 0)
+        return count;
+
+    return digitcount2(n / 10, count + 1);
+}
+
 int main()
 {
     int n, count;
     cin >> n;
     cout << digitcount(n, 0);
+    cout << digitcount2(n, 0);
     return 0;
 }
--- Assignment - 14/06.cpp
+++ Assignment - 14/06.cpp
@@ -22,3 +22,23 @@ int main()
         cout << arr[i] << endl;
     return 0;
 }
+
+int main2()
+{
+    int arr[10];
+    for (int i = 0; i < 10; i++)
+        cin >> arr[i];
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+    for (int i = 0; i < 10; i++)
+        cout << arr[i] << endl;
+    return 0;
+}
