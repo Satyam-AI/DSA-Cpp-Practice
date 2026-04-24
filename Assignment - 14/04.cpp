21746295f1a0366c685769ad244e3678776e16c4
Satyam Pandey <100727142+Satyam-AI@users.noreply.github.com>
2026-04-24 23:58:21 +0530

    find greatest and smallest number in an array

--- Assignment - 14/04.cpp
+++ Assignment - 14/04.cpp
@@ -1,1 +1,19 @@
 // 4. Write a program to find the greatest number stored in an array of size 10. Take array values from the user.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[10], greatest;
+    for (int i = 0; i < 10; i++)
+        cin >> a[i];
+    greatest = a[0];
+    for (int i = 1; i < 10; i++)
+    {
+        if (a[i] > greatest)
+            greatest = a[i];
+    }
+    cout << greatest;
+    return 0;
+}
--- Assignment - 14/05.cpp
+++ Assignment - 14/05.cpp
@@ -1,1 +1,19 @@
 // 5. Write a program to find the smallest number stored in an array of size 10. Take array values from the user
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[10], smallest;
+    for (int i = 0; i < 10; i++)
+        cin >> a[i];
+    smallest = a[0];
+    for (int i = 1; i < 10; i++)
+    {
+        if (a[i] < smallest)
+            smallest = a[i];
+    }
+    cout << smallest;
+    return 0;
+}
