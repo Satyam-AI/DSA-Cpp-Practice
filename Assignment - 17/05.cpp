d2fa3fa077354284b1d8cb6c0746a9a034c9fa23
Satyam Pandey <100727142+Satyam-AI@users.noreply.github.com>
2026-05-02 23:56:44 +0530

    Addedd solution: convert string into upper/lowercase

---
+++ Assignment - 17/05.cpp
@@ -0,0 +1,20 @@
+// 5. Write a program to convert a given string into lowercase
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    for(int i = 0; s[i] != '\0'; i++)
+    {
+        if(s[i] >= 'A' && s[i] <= 'Z')
+            s[i] = s[i] + 32;
+    }
+
+    cout << s;
+
+    return 0;
+}
