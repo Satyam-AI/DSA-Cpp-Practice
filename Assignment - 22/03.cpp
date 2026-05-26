81152f10c476442805142d8f686a5903459b661d
Satyam Pandey <100727142+Satyam-AI@users.noreply.github.com>
2026-05-26 14:38:20 +0530

    added solution to calulate sum using malloc and free

---
+++ Assignment - 22/03.cpp
@@ -0,0 +1,29 @@
+// 3. Write a program to calculate the sum of n numbers entered by the user using malloc and free
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    int *p;
+
+    p = (int*) malloc(n * sizeof(int));
+
+    int sum = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> p[i];
+        sum = sum + p[i];
+    }
+
+    cout << sum;
+
+    free(p);
+
+    return 0;
+}
