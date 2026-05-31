18a01ff1762b0b2439e524ed0a9bff6addeeee25
Satyam Pandey <100727142+Satyam-AI@users.noreply.github.com>
2026-05-31 19:12:10 +0530

    added demonstration of memory leak

---
+++ Assignment - 22/07.cpp
@@ -0,0 +1,17 @@
+// 7. Write a program to demonstrate memory leak in C.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int *p;
+
+    p = (int*) malloc(5 * sizeof(int));
+
+    p[0] = 10;
+
+    // free(p); missing
+
+    return 0;
+}
