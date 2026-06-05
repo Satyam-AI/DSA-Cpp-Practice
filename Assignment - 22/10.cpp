955d440da2e7efa8258da135ab6ff22f6524fb9e
Satyam Pandey <100727142+Satyam-AI@users.noreply.github.com>
2026-06-05 15:05:53 +0530

    Added solution to calculate maximum and minimum using DMA

---
+++ Assignment - 22/10.cpp
@@ -0,0 +1,38 @@
+// 10. Find out the maximum and minimum from an array using dynamic memory allocation in C.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    int *p;
+
+    p = (int*) malloc(n * sizeof(int));
+
+    for(int i = 0; i < n; i++)
+        cin >> p[i];
+
+    int max = p[0];
+    int min = p[0];
+
+    for(int i = 1; i < n; i++)
+    {
+        if(p[i] > max)
+            max = p[i];
+
+        if(p[i] < min)
+            min = p[i];
+    }
+
+    cout << "Maximum = " << max << "\n";
+
+    cout << "Minimum = " << min;
+
+    free(p);
+
+    return 0;
+}
