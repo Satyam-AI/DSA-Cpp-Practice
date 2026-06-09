c931499086c0bd4e440011bb9810c26b1ed0e42c
Satyam Pandey <100727142+Satyam-AI@users.noreply.github.com>
2026-06-09 14:42:34 +0530

    Added solution for A-23/03

---
+++ Assignment - 23/03.cpp
@@ -0,0 +1,14 @@
+// 3. Write a C++ program to calculate the sum of two numbers
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    int sum;
+    cin >> a >> b;
+    sum = a + b;
+    cout << sum;
+    return 0;
+}
