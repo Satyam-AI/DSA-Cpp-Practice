17924dcf04dcf7e9aa70630fbc64e63e563b5f53
Satyam Pandey <100727142+Satyam-AI@users.noreply.github.com>
2026-06-10 14:55:25 +0530

    added solution for basic c++ problem

---
+++ Assignment - 23/04.cpp
@@ -0,0 +1,14 @@
+// 4. Write a C++ program to calculate the area of a circle
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int r;
+    double area;
+    cin >> r;
+    area = (22.0 / 7 * r * r);
+    cout << area;
+    return 0;
+}
