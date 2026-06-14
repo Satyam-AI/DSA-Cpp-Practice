30a42e3b08db03ec5b7c314731bdff1dbe94ea95
Satyam Pandey <100727142+Satyam-AI@users.noreply.github.com>
2026-06-14 23:03:00 +0530

    added solution to cal. square of a number

---
+++ Assignment - 23/07.cpp
@@ -0,0 +1,14 @@
+// 7. Write a C++ program to calculate the square of a number
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+
+    int square = a * a;
+    cout << square;
+    return 0;
+}
