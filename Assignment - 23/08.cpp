a98eaa032299f2896c9e5bd318f5907774d9db2c
Satyam Pandey <100727142+Satyam-AI@users.noreply.github.com>
2026-06-15 23:59:46 +0530

    swap

---
+++ Assignment - 23/08.cpp
@@ -0,0 +1,15 @@
+// 8. Write a C++ program to swap values of two int variables without using third variable
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+   int temp =b;
+   b = a;
+   a = temp;
+   cout<<a<<b;
+   return 0;
+}
