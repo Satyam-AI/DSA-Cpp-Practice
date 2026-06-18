f520af041e5cfd19e47898bcd653b90926c2d7a2
Satyam Pandey <100727142+Satyam-AI@users.noreply.github.com>
2026-06-18 23:47:14 +0530

    to find max of two nums

---
+++ Assignment - 23/09.cpp
@@ -0,0 +1,20 @@
+// 9. Write a C++ program to find the maximum of two numbers
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cin>>a>>b;
+    if (a > b)
+    {
+        cout << a;
+    }
+    else
+    {
+        cout << b;
+    }
+    return 0;
+}
