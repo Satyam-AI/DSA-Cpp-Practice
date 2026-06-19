199e39ce3149afd053c042ec1564034109ee5b59
Satyam Pandey <100727142+Satyam-AI@users.noreply.github.com>
2026-06-19 23:59:39 +0530

    sum of all numbers of an array

---
+++ Assignment - 23/10.cpp
@@ -0,0 +1,15 @@
+// 10. Write a C++ program to add all the numbers of an array of size 10
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i =0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+}
